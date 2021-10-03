3ffdf947f54a37d87f394dcb9820cac44861a013
Sullied Security <sulliedeclat@gmail.com>
2021-10-02 23:56:00 -0400

    Changed package name
    Changed package name to be more unique

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "Location",
+    name: "SulliedLocation",
     platforms: [
         .iOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Location",
-            targets: ["Location"]),
+            name: "SulliedLocation",
+            targets: ["SulliedLocation"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,10 +22,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Location",
+            name: "SulliedLocation",
             dependencies: []),
         .testTarget(
-            name: "LocationTests",
-            dependencies: ["Location"]),
+            name: "SulliedLocationTests",
+            dependencies: ["SulliedLocation"]),
     ]
 )
