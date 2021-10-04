5533bee98c3951d58c1b6ba1c48d909eb1697ddc
Sullied Security <sulliedeclat@gmail.com>
2021-10-04 18:58:34 -0400

    Changed service view to be conditional on state
    Added toggle for location service updating bound to model
    Added message for CLLocationManger.locationServicesEnabled() == false

--- Sources/SulliedLocation/Model/LocationServicesModel.swift
+++ Sources/SulliedLocation/Model/LocationServicesModel.swift
@@ -2,6 +2,7 @@ import Foundation
 
 public class LocationServicesModel: ObservableObject {
     @Published var enabled = false
+    @Published var updating = false
     @Published var authorization: String = "Not determined"
     @Published var error: String = "nil"
 }
--- Sources/SulliedLocation/View/LocationServiceView.swift
+++ Sources/SulliedLocation/View/LocationServiceView.swift
@@ -2,21 +2,37 @@ import SwiftUI
 
 public struct LocationServiceView: View {
     @ObservedObject var model: LocationServicesModel
+    var controller: LocationControllerProtocol
+    var always: Bool
+    var background: Bool
     public var body: some View {
-        VStack(alignment: .leading) {
-            Text("Enabled: \(model.enabled ? "True" : "False")")
-            Text("Authorization: \(model.authorization)")
-            Text("Error: \(model.error)")
+        if model.enabled {
+            VStack(alignment: .leading) {
+                Text("Authorization: \(model.authorization)")
+                Text("Error: \(model.error)")
+                Toggle("Update location", isOn: $model.updating)
+                    .onChange(of: model.updating) { value in
+                        NSLog("DEBUG: Toggle model updating \(model.updating) to \(value)")
+                        if model.updating != value {
+                            controller.toggle(always: always, background: background)
+                        }
+                    }
+            }
+        } else {
+            Text("Need to enable location services in settings")
         }
     }
 
-    public init(model: LocationServicesModel) {
+    public init(model: LocationServicesModel, controller: LocationControllerProtocol, always: Bool = false, background: Bool = false) {
         self.model = model
+        self.controller = controller
+        self.always = always
+        self.background = background
     }
 }
 
 struct LocationServiceView_Previews: PreviewProvider {
     static var previews: some View {
-        LocationServiceView(model: DesignTimeLocationModel.populatedServiceModel())
+        LocationServiceView(model: DesignTimeLocationModel.populatedServiceModel(), controller: DesignTimeLocationController())
     }
 }
