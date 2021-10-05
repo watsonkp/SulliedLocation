4e128c9272c5a44d57b438256bca23bbbfcb667e
Sullied Security <sulliedeclat@gmail.com>
2021-10-04 20:26:57 -0400

    Removed controller interaction from LocationView
    Removed button with toggle interaction from LocationView

--- Sources/SulliedLocation/View/LocationView.swift
+++ Sources/SulliedLocation/View/LocationView.swift
@@ -1,25 +1,17 @@
 import SwiftUI
 
 public struct LocationView: View {
-    let controller: LocationControllerProtocol
     @ObservedObject var model: LocationModel
     public var body: some View {
-        VStack {
-            Button(action: { controller.toggle(always: false, background: false) }) {
-                Text("\(model.updating ? "Stop" : "Start")")
-            }
-            Text("\(model.description)")
-        }
+        Text("\(model.description)")
     }
-    public init(controller: LocationControllerProtocol, model: LocationModel) {
-        self.controller = controller
+    public init(model: LocationModel) {
         self.model = model
     }
 }
 
 struct LocationView_Previews: PreviewProvider {
     static var previews: some View {
-        LocationView(controller: DesignTimeLocationController(),
-                     model: DesignTimeLocationModel.populatedModel())
+        LocationView(model: DesignTimeLocationModel.populatedModel())
     }
 }
